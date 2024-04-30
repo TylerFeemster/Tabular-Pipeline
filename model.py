7ba6c9a7e36bc8fbc68033388e333b47a4aa0c6d
Tyler Feemster <ttfeemster@gmail.com>
2024-04-30 01:27:30 -0400

    early, basic skeleton

---
+++ model.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+import sklearn
+import xgboost as xgb
+import numpy as np
+from prep import Preparation
+
+class Model:
+    def __init__(self, data : Preparation):
+        pass
+
+    def fit(self):
+        pass
+
