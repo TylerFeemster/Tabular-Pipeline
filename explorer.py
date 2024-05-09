3a2000244c6c3ff3428c39bc84156bdab69b42c5
Tyler Feemster <ttfeemster@gmail.com>
2024-05-09 14:04:40 -0400

    Explorer class finished

--- explorer.py
+++ explorer.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from utils import separator, title, subtitle, align_integer
+from utils import title, subtitle, align_integer
 
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
